a9f6dc8eb8c960610c6f011ab9397a76ffa47e7b
MatiasTarta <matias.tartaglia@est.fi.uncoma.edu.ar>
2025-09-25 19:25:03 -0300

    Aumentada2

---
+++ tp2/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const boton = document.getElementById("botonAumentada");
+  boton.addEventListener("click", () => {
+    window.location.href = "./aumentada.html"; // cambia por tu archivo destino
+  });
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const boton = document.getElementById("botonCuadriculada");
+  boton.addEventListener("click", () => {
+    window.location.href = "./cuadriculada.html"; // cambia por tu archivo destino
+  });
+});
