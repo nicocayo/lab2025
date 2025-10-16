a40649423cdb753fee43956429f9907861446d2f
Nicocayo <nicolasc00@gmail.com>
2025-10-16 15:54:56 -0300

    cambio de proyecto para la web

--- tp1/sinBrdige.java
+++
@@ -1,9 +0,0 @@
-abstract class Arma { abstract void atacar(); }
-
-class EspadaFisica extends Arma {
-    void atacar() { System.out.println("Espadazo fisico"); }
-}
-
-class EspadaMagica extends Arma {
-    void atacar() { System.out.println("Espada encantada"); }
-}
